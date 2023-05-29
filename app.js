3452ef31d2784946ce21ef9e33abe9a5307bc7d0
ivandonatongua25 <ivandonatongua@gmail.com>
2023-05-29 07:30:47 -0300

    algunas rutas

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const express = require('express')
 const app = express()
 const port = 3002
 
-app.get('/h', (req, res) => {
-  res.send('Hello World!')
+app.get('/hotel/', (req, res) => {
+  res.send('Estos son los hoteles disponibles!')
 });
+
+app.get('/hotel/:id', (req, res) =>
+{
+  res.send('el hotel bueno');
+}
+);
+
+app.get('/huesped/:id', (req, res) =>{
+  res.send('El huesped');
+});
+
+app.get('/huesped/', (req, res) =>{
+  res.send('Lista de huespedes')
+})
 
 app.get('/', (req, res) => {
     res.send('Hola ivan!')
